ee7cc55e0504429927cfe4d4eb116d8701f0247b
Belete Bogale <beletebogale2@gmail.com>
2023-10-12 22:33:49 +0300

    chnages

---
+++ contact_us.php
@@ -0,0 +1,22 @@
+
+
+<?php
+echo "<h1> POST variable </h1>";
+print "<pre>";
+print_r ($_POST);
+
+
+echo "<br> <h1> GET variable </h1>";
+print "<pre>";
+print_r ($_POST);
+
+// $_REQUEST 
+// php $_REQUEST is a php super global variable which is used to collect data after submitting an HTML form. it is used to hold both GET and POST variable
+
+echo "<br> <h1> REQUEST variable </h1>";
+print "<pre>";
+print_r ($_POST);
+
+//$_FILES 
+
+?>
--- index.php
+++ index.php
@@ -1,18 +1,64 @@
+
+  
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php project</title>
-
-
+  <title>Contact Us</title>
 </head>
 <body>
-    <h1> PHP Evangadi class </h1>
 
-    <?php
+<?php
     echo date("Y-m-d");
-    echo "What is your name_______";
+   
     ?>
+  <h1>Contact Us</h1>
+  
+  <form action="contact_us.php" method="POST">
+    <label for="first_name">First Name:</label>
+    <input type="text" id="first_name" name="first_name" required><br><br>
+    
+    <label for="last_name">Last Name:</label>
+    <input type="text" id="last_name" name="last_name" required><br><br>
+    
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
+    
+    <input type="submit" value="Submit">
+  </form>
+
+  <h1>global variables </h1>
+
+  
+<!-- supper global variables example  -->
+
+<?php  
+
+$a = "alex";
+$b =  2;
+
+print "<pre>";
+print_r($GLOBALS);
+
+?>
+<br>
+
+<h1> server variable in php</h1>
+
+
+<!-- supper global variables example  -->
+
+<?php  
+
+
+print "<pre>";
+print_r($_SERVER);
+
+?>
+
+
+
+
+<br>
 </body>
 </html>
