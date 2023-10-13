4f8c2764a36982d2430af9ecae133160e3de97b4
Belete Bogale <beletebogale2@gmail.com>
2023-10-13 14:32:04 +0300

    chnages there

--- contact_us.php
+++ contact_us.php
@@ -1,22 +1,10 @@
 
 
 <?php
-echo "<h1> POST variable </h1>";
-print "<pre>";
-print_r ($_POST);
+// session_start(); // session access from index.php
+// print"<pre>";
+// print_r($_SESSION);
 
 
-echo "<br> <h1> GET variable </h1>";
-print "<pre>";
-print_r ($_POST);
-
-// $_REQUEST 
-// php $_REQUEST is a php super global variable which is used to collect data after submitting an HTML form. it is used to hold both GET and POST variable
-
-echo "<br> <h1> REQUEST variable </h1>";
-print "<pre>";
-print_r ($_POST);
-
-//$_FILES 
 
 ?>
--- index.php
+++ index.php
@@ -27,34 +27,80 @@
     <input type="submit" value="Submit">
   </form>
 
-  <h1>global variables </h1>
+  <!-- <h1>global variables </h1> -->
 
   
 <!-- supper global variables example  -->
 
 <?php  
 
-$a = "alex";
-$b =  2;
+// $a = "alex";
+// $b =  2;
 
-print "<pre>";
-print_r($GLOBALS);
+// print "<pre>";
+// print_r($GLOBALS);
 
 ?>
 <br>
 
-<h1> server variable in php</h1>
+<!-- <h1> server variable in php</h1> -->
 
 
 <!-- supper global variables example  -->
 
-<?php  
+ <?php  
+
+// echo "<h1> POST variable </h1>";
+// print "<pre>";
+// print_r ($_POST);
 
 
-print "<pre>";
-print_r($_SERVER);
+// echo "<br> <h1> GET variable </h1>";
+// print "<pre>";
+// print_r ($_GET);
 
-?>
+// $_REQUEST 
+// php $_REQUEST is a php super global variable which is used to collect data after submitting an HTML form. it is used to hold both GET and POST variable
+
+// echo "<br> <h1> REQUEST variable </h1>";
+// print "<pre>";
+// print_r ($_REQUEST);
+
+//$_FILES 
+
+
+//cookies and session 
+//cookies is a small that the server embeds on the user,s computer. originally, cookies are created with the intention of improving your experience when visiting a website. these small files saved on your browser help developers give more personal, convenient website visits. on the other hand cookies can also be used to spy on you. 
+
+//>> a cookies is created with the setcookies()
+
+
+
+// print "<pre>";
+// print_r($_SERVER);
+
+// $cookie_name ="test_cookie";
+// $cookie_value ="visited content page";
+// setcookie($cookie_name, $cookie_value, time()+(86400*30), "/");
+
+// // print cookie
+
+// print"<pre>";
+// print_r($_COOKIE);
+
+
+
+
+// //SESSION 
+// session_start(); // session should be start before used it 
+// print"<pre>";
+// print_r($_SESSION);
+
+// $_SESSION["test"] = 'alex';
+
+//>> we can access the session on the other page. 
+
+?> 
 
 
 
