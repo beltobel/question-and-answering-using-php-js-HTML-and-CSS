874ee1e89a0a6ca6aad1013e1005013e9b70a094
beltobel <beletebogale2@gmail.com>
2023-10-29 10:18:21 +0300

    php insert

--- QA/user/registrationHandler.php
+++ QA/user/registrationHandler.php
@@ -2,7 +2,28 @@
 
 require_once('../helpers/init.php');
 
+$fname = "";
+$lname ="";
+$email = "";
+$password = "";
 
-test();
-echo "test 2";
+//grab values from user interface 
+// Retrieve form values
+if (isset($_POST['firstName'])) 
+{
+ $firstName = $_POST['firstName'];
+}
+if(isset($_POST['lastName']))
+{
+    $lastName = $_POST['lastName'];
+}
+if(isset($_POST['email']) )
+{
+    $email = $_POST['email'];
+
+}
+if(isset($_POST['password'])) {
+   
+    $password = $_POST['password'];
+}
 ?>
